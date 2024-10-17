071bcb5ddfb8d8db53bac5b7ee7c11d1b2f749ca
Yu Cheng Chang <116697928+Gotcodetalent@users.noreply.github.com>
2024-10-17 15:45:39 +0800

    Delete math directory

--- math/start_probe.sh
+++
@@ -1,4 +0,0 @@
-h1 python /home/p4/p4-tools/p4-learning/exercises/08-Simple_Routing/p4runtime/hula/probe.py &
-h2 python /home/p4/p4-tools/p4-learning/exercises/08-Simple_Routing/p4runtime/hula/probe.py &
-h3 python /home/p4/p4-tools/p4-learning/exercises/08-Simple_Routing/p4runtime/hula/probe.py &
-h4 python /home/p4/p4-tools/p4-learning/exercises/08-Simple_Routing/p4runtime/hula/probe.py &
